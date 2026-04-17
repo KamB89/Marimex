33f6c397a1c86863e28f1774d44829a63ccdefe7
Kamran <kamran.beybutovv@gmail.com>
2026-04-17 20:12:11 +0200

    Step 1

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import './app.css';
+import AppInfo from '../app-info/app-info.js';
+
+
+function App(){
+    return(
+   <div className="app">
+   <AppInfo/>
+
+   </div>
+
+    )
+}
+
+export default App
